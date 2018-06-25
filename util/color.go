30760f4247b141745663ad93d03e9f278fd7e4cd
Erin Grasmick <egrasmick@wisc.edu>
2018-06-26 00:32:16 +0200

    shitpost durability fix and color support added

---
+++ util/color.go
@@ -0,0 +1,25 @@
+package util
+
+var colors = map[string]string{
+	"NORMAL":     "\u000f",
+	"BOLD":       "\u0002",
+	"UNDERLINE":  "\u001f",
+	"REVERSE":    "\u0016",
+	"WHITE":      "\u00030",
+	"BLACK":      "\u00031",
+	"DARK_BLUE":  "\u00032",
+	"DARK_GREEN": "\u00033",
+	"RED":        "\u00034",
+	"BROWN":      "\u00035",
+	"GREEN":      "\u00039",
+}
+
+func Greentext(colorable string) string {
+	result := colors["GREEN"] + colorable
+	return result
+}
+
+func Normaltext(colorable string) string {
+	result := colors["NORMAL"] + colorable
+	return result
+}
