e754e0bfcf42f3478bd327fee0d67951f854c0eb
boreq <boreq@0x46.net>
2018-07-20 22:34:14 +0200

    Add quotes

--- core/list.go
+++ core/list.go
@@ -5,11 +5,17 @@ import (
 	"github.com/lovelaced/glitzz/modules"
 	"github.com/lovelaced/glitzz/modules/info"
 	"github.com/lovelaced/glitzz/modules/pipes"
+	"github.com/lovelaced/glitzz/modules/quotes"
 )
 
-func CreateModules(sender modules.Sender, conf config.Config) []modules.Module {
+func CreateModules(sender modules.Sender, conf config.Config) ([]modules.Module, error) {
 	var rv []modules.Module
 	rv = append(rv, info.New(sender, conf))
 	rv = append(rv, pipes.New(sender, conf))
-	return rv
+	if m, err := quotes.New(sender, conf); err != nil {
+		return nil, err
+	} else {
+		rv = append(rv, m)
+	}
+	return rv, nil
 }
