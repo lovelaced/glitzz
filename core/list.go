08a0ac0fe64bd19e33a47aa8ceb28816d2f5133c
boreq <boreq@0x46.net>
2018-07-22 18:55:00 +0200

    Add shitpost

--- core/list.go
+++ core/list.go
@@ -4,6 +4,7 @@ import (
 	"github.com/lovelaced/glitzz/config"
 	"github.com/lovelaced/glitzz/modules"
 	"github.com/lovelaced/glitzz/modules/c3"
+	"github.com/lovelaced/glitzz/modules/fourchan"
 	"github.com/lovelaced/glitzz/modules/info"
 	"github.com/lovelaced/glitzz/modules/pipes"
 	"github.com/lovelaced/glitzz/modules/quotes"
@@ -14,6 +15,7 @@ func CreateModules(sender modules.Sender, conf config.Config) ([]modules.Module,
 	rv = append(rv, info.New(sender, conf))
 	rv = append(rv, pipes.New(sender, conf))
 	rv = append(rv, c3.New(sender, conf))
+	rv = append(rv, fourchan.New(sender, conf))
 	if m, err := quotes.New(sender, conf); err != nil {
 		return nil, err
 	} else {
