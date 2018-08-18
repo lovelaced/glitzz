5ca03db16dfaeae58dc8155d0e0b101377394704
boreq <boreq@0x46.net>
2018-08-18 23:46:35 +0200

    Add decide

---
+++ modules/decide/decide.go
@@ -0,0 +1,39 @@
+package decide
+
+import (
+	"fmt"
+	"github.com/lovelaced/glitzz/config"
+	"github.com/lovelaced/glitzz/core"
+	"github.com/lovelaced/glitzz/util"
+	"strings"
+)
+
+func New(sender core.Sender, conf config.Config) (core.Module, error) {
+	rv := &decide{
+		Base: core.NewBase("decide", sender, conf),
+	}
+	rv.AddCommand("decide", rv.decide)
+	return rv, nil
+}
+
+type decide struct {
+	core.Base
+}
+
+func (d *decide) decide(arguments core.CommandArguments) ([]string, error) {
+	if len(arguments.Arguments) > 0 {
+		s := strings.Join(arguments.Arguments, " ")
+		elements := strings.Split(s, " or ")
+		if len(elements) < 2 {
+			return nil, nil
+		}
+		element, err := util.GetRandomArrayElement(elements)
+		if err != nil {
+			d.Log.Debug("get random array element failed", "err", err, "arguments", arguments)
+			return nil, nil
+		}
+		text := fmt.Sprintf("%s: %s", arguments.Nick, element)
+		return []string{text}, nil
+	}
+	return nil, nil
+}
