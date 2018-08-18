ca57ef96ec1a979191c6101ffbb221782a211b61
boreq <boreq@0x46.net>
2018-08-18 23:46:35 +0200

    Add decide

---
+++ modules/decide/decide_test.go
@@ -0,0 +1,52 @@
+package decide
+
+import (
+	"github.com/lovelaced/glitzz/config"
+	"github.com/lovelaced/glitzz/core"
+	"testing"
+)
+
+func TestDecideNoParameters(t *testing.T) {
+	p, err := New(nil, config.Default())
+	if err != nil {
+		t.Fatalf("error creating module %s", err)
+	}
+
+	output, err := p.RunCommand(core.Command{Text: ".decide"})
+	if err != nil {
+		t.Errorf("error was not nil %s", err)
+	}
+	if len(output) != 0 {
+		t.Errorf("output length %d", len(output))
+	}
+}
+
+func TestDecideMissingOr(t *testing.T) {
+	p, err := New(nil, config.Default())
+	if err != nil {
+		t.Fatalf("error creating module %s", err)
+	}
+
+	output, err := p.RunCommand(core.Command{Text: ".decide another thing"})
+	if err != nil {
+		t.Errorf("error was not nil %s", err)
+	}
+	if len(output) != 0 {
+		t.Errorf("invalid output length %d", len(output))
+	}
+}
+
+func TestDecide(t *testing.T) {
+	p, err := New(nil, config.Default())
+	if err != nil {
+		t.Fatalf("error creating module %s", err)
+	}
+
+	output, err := p.RunCommand(core.Command{Text: ".decide thing or another thing or something else", Nick: "nick"})
+	if err != nil {
+		t.Errorf("error was not nil %s", err)
+	}
+	if len(output) != 1 {
+		t.Errorf("invalid output length %d", len(output))
+	}
+}
